1ae9f961914441e5049f6f11fa71d22b2e2a79fc
Ahmad Ibraheem <Ahmad.ibraheemse@gmail.com>
2026-06-15 15:36:11 +0300

    Add gh-pages for deployment and update Vite config base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base value if the "/repoName/"
+  base: '/test-undp/',
   plugins: [react()],
   build: {
     cssTarget: 'safari13',
